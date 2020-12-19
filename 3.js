c49f405591216a6d8051418aa6f50b1d399ce852
lightkun10 <lifeisubw@gmail.com>
2020-12-19 13:31:42 +0700

    Hapus file

--- 3.js
+++
@@ -1,9 +0,0 @@
-function cetakPola(num) {
-  for (let row = 0; row < num; row++) { // setiap row
-    for (let kiri = row+1; kiri < row; kiri++) {
-      process.stdout.write(kiri); 
-    }
-  }
-}
-
-cetakPola(10);
